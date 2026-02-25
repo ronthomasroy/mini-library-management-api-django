ffd04bb46f2efbc8eeda6a1a475441bcfb1eeb0d
ronthomasroy <ronroy003@gmail.com>
2026-02-25 11:23:58 +0530

    fixed borrow requests

--- library/views.py
+++ library/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status, generics
+from rest_framework import viewsets, status
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
